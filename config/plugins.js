dd5a28a168680a17799fb3ab40292c516f4eb2e2
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-12-18 21:29:58 -0300

    teste

--- config/plugins.js
+++ config/plugins.js
@@ -30,3 +30,44 @@ module.exports = ({ env }) => ({
   },
   // ...
 });
+
+
+
+module.exports = ({ env }) => ({
+  init(providerOptions) {
+    // init your provider if necessary
+    var admin = require("firebase-admin"),
+
+    // Fetch the service account key JSON file contents
+    var serviceAccount = require("../firebase-adminsdk-hetfg.json"),
+
+    // Initialize the app with a service account, granting admin privileges
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://letmeask-web-nlw-default-rtdb.firebaseio.com"
+    }),
+
+    var db = admin.database();
+    var ref = db.ref("restricted_access/secret_document");
+    ref.once("value", function(snapshot) {
+      console.log(snapshot.val());
+      
+    });
+
+
+    return {
+      upload(file) {
+        // upload the file in the provider
+        provider: "firebase-storage",
+        providerOptions: {
+          serviceAccount: require("../firebase-adminsdk-hetfg.json"),
+          bucketUrl: env( "STORAGE_BUCKET_URL" ),
+        },
+
+      },
+      delete(file) {
+        // delete the file in the provider
+      },
+    };
+  },
+});
